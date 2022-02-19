f556168cc30b08cff3a5142fcc992f99133f8dfb
jubiiz <jubiiz.audet@gmail.com>
2022-02-19 12:06:12 -0500

    added full multi sensor v0 and 1; none funcitonal yet (not tested at all)

--- multi/full_0/full_0.ino
+++ multi/full_0/full_0.ino
@@ -1,86 +1,79 @@
 #include <Wire.h>
 #include <SPI.h>
+#include "helper.h"
+
 
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <Adafruit_CCS811.h>
 
-//for faster prints
-#define pr(msg) Serial.print(msg)
-#define prln(msg) Serial.println(msg)
-
-//if you need to read altitude,you need to know the sea level pressure
 #define SEALEVELPRESSURE_HPA (1013.25)
+#define CD_UPDATE_GAP 3000
 
-//This Macro definition decide whether you use I2C or SPI
-//When USEIIC is 1 means use I2C interface, When it is 0,use SPI interface
-#define USEIIC 1
 #define LEDPIN 2
 
-/*
-This Demo is tested on UNO PLUS
-SPI:
-SPI_SCK:  D13
-SPI_MISO: D12
-SPI_MOSI: D11
-SPI_CS:   D10
+Adafruit_BME280 bme;
+Adafruit_CCS811 ccs;
 
-I2C:
-I2C_SCL:  A5
-I2C_SDA:  A4
-the default I2C address is 0x77, you can change it in Adafruit_BME280.h
-*/
-
-#if(USEIIC)
-  Adafruit_BME280 bme;
-#else
-  #define SPI_SCK 13
-  #define SPI_MISO 12
-  #define SPI_MOSI 11
-  #define SPI_CS 10
-  Adafruit_BME280 bme(SPI_CS, SPI_MOSI, SPI_MISO, SPI_SCK);
-#endif
-
-unsigned long delayTime;
+//setting up chute update
 int *pcounter;
 float *psum;
 float *pstart_time;
 float *plast_alt;
-void setup() {
 
+void setup() {
     Serial.begin(9600);
     pinMode(LEDPIN, OUTPUT); 
+    
+    // start BME sensor
     bool rslt;
     rslt = bme.begin();  
     if (!rslt) {
-        Serial.println("Init Fail,Please Check your address or the wire you connected!!!");
+        Serial.println("BME280 Init Fail,Please Check your address or the wire you connected!!!");
         while (1);
-    }
-  
-    Serial.println("Init Success");
-    deployChute();
+    }  
+    Serial.println("BME280 Init Success");
 
+    // start ccs811 sensor
+    rslt = ccs.begin();
+    if (!rslt) {
+      Serial.println(" CCS811 Init Fail,Please Check your address or the wire you connected!!!");
+      while(1);
+    }
+    Serial.println("CCS811 Init success");
+
+    // init BME chute deploy variables  
     int counter = 0;
     float sum = 0;
     float start_time = millis();
     float last_alt = bme.readAltitude(SEALEVELPRESSURE_HPA);
-
     pcounter = &counter;
     psum = &sum;
     pstart_time = &start_time;
     plast_alt = &last_alt;
     *plast_alt = bme.readAltitude(SEALEVELPRESSURE_HPA);
-    
-    //update_altitude(&counter, &sum, &last_alt, &start_time);
 }
 
-void loop() { 
+void loop() {
+
+    log_data();
     update_altitude(pcounter, psum, plast_alt, pstart_time);
     delay(20);
+
 }
+
+void log_data(){
+  //TODO
+
+  // order: 
+  // altitude, pressure, temperature, humidity, x_a, y_a, z_a, x_g, y_g, z_g, 
+  
+}
+
 
 void update_altitude(int *pcounter, float *psum, float *plast_alt, float *pstart_time) {
     float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-    if(millis()-*pstart_time >3000){ //if it's been more than 3 seconds
+    if(millis()-*pstart_time >CD_UPDATE_GAP){ //if it's been more than CD_UPDATE_GAP seconds
       // show the average value of the altitude over 1 sec
       pr("counter: ");
       prln(*pcounter);
@@ -94,7 +87,6 @@ void update_altitude(int *pcounter, float *psum, float *plast_alt, float *pstart
       if(diff < -1){
         deployChute();
       }
-
       //when all this is done, indicate what counter was at, update last_alt (=alt) reset counter, start_time, avg
       pr("previous counter number: ");
       pr(*pcounter);
@@ -103,24 +95,11 @@ void update_altitude(int *pcounter, float *psum, float *plast_alt, float *pstart
       *plast_alt = avg;
       *pcounter = 1;
       *pstart_time = millis();
-      *psum = 0;
-      
+      *psum = 0;      
     }
-
     //update the altitude avg, then counter
     *psum += altitude;
-    *pcounter += 1;
-
-/*
-    pr(" sum/counter, last avg, sum, counter");
-    pr(*sum/(*counter));
-    pr(" ");
-    pr(*last_alt);
-    pr(" ");
-    pr(*sum);
-    pr(" ");
-    prln(*counter);
-    */   
+    *pcounter += 1;   
 }
 
 void deployChute(){
---
+++ multi/full_1/full_1.ino
@@ -0,0 +1,191 @@
+#include <Wire.h>
+#include <SPI.h>
+#include "helper.h"
+
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_CCS811.h>
+#include <Adafruit_MPU6050.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+#define CD_UPDATE_GAP 3000
+
+#define LEDPIN 2
+
+Adafruit_BME280 bme;
+Adafruit_CCS811 ccs;
+Adafruit_MPU6050 mpu;
+
+//setting up chute update
+int *pcounter;
+float *psum;
+float *pstart_time;
+float *plast_alt;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(LEDPIN, OUTPUT); 
+    
+    // start BME sensor
+    bool rslt;
+    rslt = bme.begin();  
+    if (!rslt) {
+        Serial.println("BME280 Init Fail,Please Check your address or the wire you connected!!!");
+        while (1);
+    }  
+    Serial.println("BME280 Init Success");
+
+    // start ccs811 sensor
+    rslt = ccs.begin();
+    if (!rslt) {
+      Serial.println(" CCS811 Init Fail,Please Check your address or the wire you connected!!!");
+      while(1);
+    }
+    Serial.println("CCS811 Init success");
+
+    // start mpu6050 sensor
+    if (!mpu.begin()) {
+      Serial.println("Failed to find MPU6050 chip");
+      while (1);
+    }
+    Serial.println("MPU6050 Init success!");
+
+    // init BME chute deploy variables  
+    int counter = 0;
+    float sum = 0;
+    float start_time = millis();
+    float last_alt = bme.readAltitude(SEALEVELPRESSURE_HPA);
+    pcounter = &counter;
+    psum = &sum;
+    pstart_time = &start_time;
+    plast_alt = &last_alt;
+    *plast_alt = bme.readAltitude(SEALEVELPRESSURE_HPA);
+
+    // settup mpu range (accel & gyro)
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+}
+
+void loop() {
+
+    log_data();
+    //update_altitude(pcounter, psum, plast_alt, pstart_time);
+    delay(20);
+
+}
+
+void log_data(){
+  //TODO
+  
+  // order: 
+  // altitude, pressure, temperature, humidity, x_a, y_a, z_a, x_g, y_g, z_g, CO2, TVOC 
+
+  // BME280 test code
+  Serial.print("temperature:");
+  Serial.print(bme.readTemperature());
+  Serial.print("*C   ");
+
+  Serial.print("pressure:");
+  Serial.print(bme.readPressure()/100.0F);
+  Serial.print("hPa   ");
+
+  Serial.print("humidity:");
+  Serial.print(bme.readHumidity());
+  Serial.print("%   ");
+
+  Serial.print("altitude:");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println("m--------------------------------BME280------------------------------------");
+
+  // mpu test code
+    /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("---------------------------------MPU6050--------------------------------------");
+
+  if(ccs.available()){
+    if(!ccs.readData()){
+      Serial.print(" CO2: ");
+      Serial.print(ccs.geteCO2());
+      Serial.print("ppm, TVOC: ");
+      Serial.println(ccs.getTVOC());
+      Serial.println("----------------------------------------CCS811--------------------------------");
+      success = true;
+    } 
+    else{
+      success = false; 
+    }
+  }
+  else{
+    success = false;
+  }
+
+  
+  delay(500);
+  
+}
+
+
+void update_altitude(int *pcounter, float *psum, float *plast_alt, float *pstart_time) {
+    float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+    if(millis()-*pstart_time >CD_UPDATE_GAP){ //if it's been more than CD_UPDATE_GAP seconds
+      // show the average value of the altitude over 1 sec
+      pr("counter: ");
+      prln(*pcounter);
+      float avg = *psum/(*pcounter);
+      pr("current altitude average: ");
+      pr(avg);
+      pr(" diff with last: ");
+      float diff = avg-*plast_alt;
+      prln(diff);
+      // then, if there has been at least a meter of drop, deploy parachute
+      if(diff < -1){
+        deployChute();
+      }
+      //when all this is done, indicate what counter was at, update last_alt (=alt) reset counter, start_time, avg
+      pr("previous counter number: ");
+      pr(*pcounter);
+      delay(2000);
+
+      *plast_alt = avg;
+      *pcounter = 1;
+      *pstart_time = millis();
+      *psum = 0;      
+    }
+    //update the altitude avg, then counter
+    *psum += altitude;
+    *pcounter += 1;   
+}
+
+void deployChute(){
+  for(int i=0; i<20; i++){
+      Serial.println("deploying ");
+      digitalWrite(LEDPIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(100);                       // wait for a second
+      digitalWrite(LEDPIN, LOW);    // turn the LED off by making the voltage LOW
+      delay(100); 
+  }
+}
